0723c7f08eaf9dfc12f9c7c79ff1e12aadc16cbb
emicanonica <emicanonica@gmail.com>
2017-03-06 12:03:38 -0300

    Titulo:Escucha.c ahora es un demon
    Desripcion:ahora una vez que se ejecuta escucha queda el proceso corriendo como un demonio (independiente del terminal)

--- mensajes/escucha.c
+++ mensajes/escucha.c
@@ -31,6 +31,45 @@ char buffer[tam];
 int main(int argc, char *argv[])
 {
 
+pid_t process_id = 0;
+pid_t sid = 0;
+
+//crea el proceso hijo
+process_id = fork();
+
+// Indication of fork() failure
+if (process_id < 0)
+{
+printf("fork failed!\n");
+// Return failure in exit status
+exit(1);
+}
+// PARENT PROCESS. Need to kill it.
+if (process_id > 0)
+{
+printf("process_id of child process %d \n", process_id);
+// return success in exit status
+exit(0);
+}
+
+//unmask the file mode
+umask(0);
+//set new session
+sid = setsid();
+if(sid < 0)
+{
+// Return failure
+exit(1);
+}
+
+// Change the current working directory to root.
+chdir("/");
+
+// Close stdin. stdout and stderr
+close(STDIN_FILENO);
+close(STDOUT_FILENO);
+close(STDERR_FILENO); 
+
 struct str_data *data;
 data = (struct str_data *) buffer;
 
