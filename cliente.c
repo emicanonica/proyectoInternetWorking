4847c7b4c54c2b7437f0888b6a2ac7e101426d71
emicanonica <emicanonica@gmail.com>
2017-06-28 11:37:46 -0300

    Titulo:agregado de funcion "setVersion"
    Desripcion: a setVersion se le arreglaron cosas para que no elimine los archivos .conf ni tabla.txt

--- cliente.c
+++ cliente.c
@@ -170,35 +170,33 @@ int main(int argc, char const *argv[]) {
     setConf(3,vt);
     //COPIAR ARCHIVOS DESDE LA UBICACION ESTABLECIDA A LA CARPETA DE LA APLICACION, LUEGO MANDAR UN MENSAJE HOLA PARA QUE ACTUALIZEN LAS TABLAS LOS OTROS USUARIOS
 		nombreusu = nombreusuario();
-		char * part2 = direccion; //direccion de la carpeta donde se guardan los archivos
+		char * part2 = strcat(direccion,"*"); //direccion de la carpeta donde se guardan los archivos
 		char part[30] = "/home/"; //direccion de la carpeta donde se versiona y comparte los archivos, carpeta oculta la que puse es de ejemplo
-		char part3[30] = "/.nombre";		
-		char comando[70];
-		char cmd1[30] = "rm -r ";
-		char cmd2[30] = "cp -r ";
+		char part3[30] = "/.NOMBRE/";
+		char comando[100];
+		char cmd1[30] = "find ";
+		char cmd2[30] = "cp -rf ";
 		strcpy(comando , cmd1);
 		strcat(comando , part);
-		strcat(comando , nombreusu); 
+		strcat(comando , nombreusu);
 		strcat(comando , part3);
-		//printf("%s \n" , comando );
-		system( comando ); // el comando quedaria "rm -r /home/nombreusuario/.nombre"
-		//printf("carpeta eliminada\n");
+    strcat(comando," ! -name '.conf' ! -name 'tabla.txt' -type f -exec rm -f {} +");
+		system( comando ); // el comando quedaria "find /home/emi/.NOMBRE/ ! -name '.conf' -type f -exec rm -f {} +"
 		strcpy(comando , cmd2);
 		strcat(comando , part2);
 		strcat(comando , " ");
 		strcat(comando , part);
-		strcat(comando , nombreusu); 
+		strcat(comando , nombreusu);
 		strcat(comando , part3);
-		//printf("%s \n" , comando );	
-		system( comando ); // el comando quedaria "cp -r carpeta visible carpeta oculta
-		//printf("carpeta copiada\n");
-    
+		system( comando ); // el comando quedaria "cp -rf "carpeta_visible" "carpeta_oculta"
+		printf("Repositorio actualizado a la versión %s\n", vt);
+
 //ayuda
   }else if (strcmp(argv[1], "help") == 0) { //setea el tiempo local como numero de la version del repositorio
 
     printf("\nUso: NOMBRE [OPTION] \n");
     printf("\nOpciones de configuracion:\n\n conf \t\t cargar nombre de usuario y ubicacion del repositorio de trabajo\n setId \t\t Ingresa nuevo nombre de usuario\n setDir \t Ingresa la nueva ubicación del repositorio de trabajo\n ");
-    printf("\nOpciones de uso:\n\n hola \t\t Unirse al grupo de proyecto\n version \t consultar version más reciente\n actualizar \t actualiza el repositorio local\n");
+    printf("\nOpciones de uso:\n\n hola \t\t Unirse al grupo de proyecto\n version \t consultar version más reciente\n actualizar \t trae los archivos de la version más actual en la red\n setVersion \t actualiza el directorio local y le asigna un número de versión");
     printf("\n\n Si es la primera vez que utiliza este programa corra en consola el comando './NOMBRE conf' para realizar la configuración inicial\n");
 
 //En caso de que se ingrese un argumento incorrecto
