375e7604d8bbc943a1258a72198caf037bf2a03a
emicanonica <emicanonica@gmail.com>
2017-07-07 16:13:49 -0300

    por fin anda el enviar archivos correctamente

--- mensajes/enviarArchivo.c
+++ mensajes/enviarArchivo.c
@@ -14,25 +14,51 @@
 
 int enviarArchivo(uint32_t ip , char * nombreArchivo)
 {
-    char * direccion = malloc(300);
-    direccion = strcat(getConf(2), nombreArchivo);
+
+    //nombreArchivo = malloc(20);
+    //char * direccion = malloc(300);
+    //direccion = strcat(getConf(2), nombreArchivo);
+
+    char AppDir[100];
+    char part[30] = "/home/";
+    char part2[30] = "/.NOMBRE/";
+    char * nombreusu = nombreusuario();
+    strcpy(AppDir , part);
+    strcat(AppDir,nombreusu);
+    strcat(AppDir,part2);
+    strcat(AppDir,nombreArchivo);
+    printf("%s\n", AppDir);
 
     int sd = 0;
     struct sockaddr_in serv_addr;
     char buffer[256];
-    int filedes;
+    int filedes=0;
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(1235);
     serv_addr.sin_addr.s_addr = ip;//inet_addr("127.0.0.1");
 
+    //memset(buffer,0,sizeof(buffer));
+
     sd = socket(AF_INET, SOCK_STREAM, 0);
 
-    connect(sd,(struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+RETRY:    if (connect(sd,(struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+      perror("connect failed error");
+      goto RETRY;
+      return 1;
+    }
+
+    //sleep(1);
+
 
     FILE *f;
-    f = fopen(direccion, "rb");
+    f = fopen(AppDir, "rb");
+    //printf("%s\n", direccion);
     filedes = fileno(f);
+    printf("aca\n");
+    printf("%d\n", filedes);
+
 
     while( read(filedes, buffer, sizeof(buffer)) > 0 ) // es decir mientras no sea fin de archivo (read devuelve 0 cuando lee fin de archivo)
     {
@@ -41,6 +67,7 @@ int enviarArchivo(uint32_t ip , char * nombreArchivo)
     }
 
     close(sd); // Esto hace que del otro lado termine el while por que el read le devuelve 0
+    printf("fin enviarArchivo\n" );
 
     return 0;
 
